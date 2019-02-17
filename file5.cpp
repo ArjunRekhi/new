a6918d9d45f93e44d630e2e145a57baceb343126
ArjunRekhi <arjunrekhi265@gmail.com>
2019-02-17 21:44:28 +0530

    add

---
+++ file5.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std
+class calculator
+{
+	public:int add(int a,intb)
+	       {
+		       a+b;
+
+	        }
+}
+int main()
+{ int a,b;
+	cout<<"enter two numbers"<<endl;
+	cin>>a;
+	cin>>b;
+	calculator o1;
+        o1.add;
+}
+
+
